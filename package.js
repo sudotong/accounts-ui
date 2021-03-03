1d3522bb27cd6fc9a766c5ea422eaef71533266e
Sam Udotong <sudotong@gmail.com>
2021-03-03 15:21:47 -0500

    chore: update api to 2.0

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('2.0');
   api.use('ecmascript');
   api.use('accounts-base');
   api.use('check');
